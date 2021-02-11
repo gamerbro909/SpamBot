ce66cbc6681c82972365cfdf632de937bd4811e4
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 05:15:08 +0000

    Added Message Feature
    Added a new option to have 'Done!' be typed so you know that the script has finished. This won't actually send, rather it will be typed out wherever you selected to send it.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import pyautogui, time
 
 # How long until the bot begins spamming.
 WaitTime = 10
+# This will type 'Done!' when the script has finished. It won't send it, rather it will show typed in the text box
+DoneMessage = 'true'
 
 time.sleep(WaitTime)
 b = open("script", 'r')
 for word in b:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+
+if DoneMessage == 'true':
+    pyautogui.typewrite("Done!")
+else: exit()
+
