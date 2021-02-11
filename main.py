7820c898a220d82c00225d5d3a3f1d859c03cf32
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 12:25:30 +0000

    Fixed a space

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # You could probably get your account auto banned for this, be careful
 import pyautogui, time
 
-
 WaitTime = 10 # How long until the bot begins spamming.
 DoneMessage = 'true' # This will type 'Done!' when the script has finished. It won't send it, rather it will show typed in the text box
 ChillZone = 'false' # For social medias which have a message cool down, mainly for Discord.
