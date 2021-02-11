b0907f5f8daf2b589b5a78a468a085bd36549c6e
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 21:59:37 +0000

    Added MessageDelay
    Added MessageDelay setting

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# You could probably get your account auto banned for this, be careful // Discord: ryann#7322
+# You could probably get your account banned or reported for this, be careful // Discord: ryann#7322
 import pyautogui
 import time
 
 WaitTime = 7  # How long until the bot begins spamming. Default = 7
 DoneMessage = 'true'  # This will type 'Done!' when the script has finished. Default = 'true'
 ChillZone = 'true'  # For social medias which have a message cool down, mainly for Discord. Default = 'true'
+MessageDelay = 0  # How many seconds between each message. Default = 0
 
 time.sleep(WaitTime)
 b = open("script.txt", 'r')
@@ -13,6 +14,7 @@ for word in b:
     if ChillZone == 'true':
         time.sleep(1.25)
 
+    time.sleep(MessageDelay)
     pyautogui.typewrite(word)
     pyautogui.press("enter")
 
