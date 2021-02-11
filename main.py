00acc1100b306c9f9b3de317d61218f54c6dd822
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 07:02:24 +0000

    Potential Fix For /'s Breaking
    * Fixed /'s breaking on certain platforms, open a bug report if theres any more issues

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# You could probably get your account auto banned for this, be careful // Discord: ryann#7322
+# You could probably get your account auto banned for this, be careful
 import pyautogui, time
 
 # How long until the bot begins spamming.
@@ -6,7 +6,11 @@ WaitTime = 10
 # This will type 'Done!' when the script has finished. It won't send it, rather it will show typed in the text box
 DoneMessage = 'true'
 # For social medias which have a message cool down, mainly for Discord.
-ChillZone = 'true'
+ChillZone = 'false'
+
+# This is a potential fix for /'s breaking on certain platforms.
+SlashFix = 'true'
+
 
 time.sleep(WaitTime)
 b = open("script", 'r')
@@ -17,6 +21,9 @@ for word in b:
 
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+
+    if SlashFix == 'true':
+        pyautogui.press("enter")
 
 if DoneMessage == 'true':
     pyautogui.typewrite("Done!")
