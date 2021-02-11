26ec8c901399351b5275d71788b3543d5ed442dd
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 21:34:58 +0000

    Fixed DoneMessage Bug
    Fixed a bug where it 'Done!' would be sent after every message
    ChillZone time set to 1.25

--- main.py
+++ main.py
@@ -5,21 +5,19 @@ import time
 WaitTime = 7  # How long until the bot begins spamming. Default = 7
 DoneMessage = 'true'  # This will type 'Done!' when the script has finished. Default = 'true'
 ChillZone = 'true'  # For social medias which have a message cool down, mainly for Discord. Default = 'true'
-ChillZoneTime = 1  # If ChillZone is enabled, this is how long the cool down is. Default = 1
 
 time.sleep(WaitTime)
-b = open("script", 'r')
+b = open("script.txt", 'r')
 
 for word in b:
     if ChillZone == 'true':
-        time.sleep(ChillZoneTime)
+        time.sleep(1.25)
 
     pyautogui.typewrite(word)
     pyautogui.press("enter")
-    pyautogui.press("enter")
 
-    if DoneMessage == 'true':
-        pyautogui.typewrite("Done!")
-        print("Done!")
+if DoneMessage == 'true':
+    pyautogui.typewrite("Done!")
+    print("Done!")
 
 exit()
