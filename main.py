c90882c09ee83c4938840ea482b8bb6b1da8f589
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 05:20:30 +0000

    Added ChillZone mode
    Mainly for discord, but this feature will try and avoid any social medias message cooldowns. Proper config is coming.

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ import pyautogui, time
 WaitTime = 10
 # This will type 'Done!' when the script has finished. It won't send it, rather it will show typed in the text box
 DoneMessage = 'true'
+# For social medias which have a message cool down, mainly for Discord.
+ChillZone = 'false'
 
 time.sleep(WaitTime)
 b = open("script", 'r')
+
 for word in b:
+    if ChillZone == 'true':
+        time.sleep(3)
+
     pyautogui.typewrite(word)
     pyautogui.press("enter")
 
 if DoneMessage == 'true':
     pyautogui.typewrite("Done!")
 else: exit()
-
