3b37e03ab2f58a196448487201c8b0607fcdbc72
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-11 12:29:03 +0000

    Updated Timings

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # You could probably get your account auto banned for this, be careful
 import pyautogui, time
 
-WaitTime = 10 # How long until the bot begins spamming.
+WaitTime = 7 # How long until the bot begins spamming.
 DoneMessage = 'true' # This will type 'Done!' when the script has finished. It won't send it, rather it will show typed in the text box
-ChillZone = 'false' # For social medias which have a message cool down, mainly for Discord.
+ChillZone = 'true' # For social medias which have a message cool down, mainly for Discord.
 ChillZoneTime = 1 # If ChillZone is enabled, this is how long the cooldown is
 SlashFix = 'true' # This is a potential fix for /'s breaking on certain platforms.
 ShowDebug = 'false' # Shows Debug Messages in console
