dae4c506e16e3ccab3f5cbd6020208b83487159f
Kumo <63556081+Mysttt@users.noreply.github.com>
2021-02-22 10:22:36 +0000

    Added Loop Setting + Quit Key

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 # You could probably get your account banned or reported for this, be careful // Discord: ryann#7322
+# Version 1.6 // Requires Config 1.1
 import pyautogui
 import time
 from ruamel.yaml import YAML
+import keyboard
 
 yaml = YAML()
 
 with open("./config.yml", "r", encoding="utf-8") as file:
     config = yaml.load(file)
 
-
 time.sleep(config['WaitTime'])
-sb = open("script.txt", 'r')
+if config['script_loop'] == True:
+    while keyboard.is_pressed(config['stop_key']) == False:
+        sb = open("script.txt", 'r')
 
-for word in sb:
-    if config['ChillZone'] == 'true':
-        time.sleep(1.7)
+        for word in sb:
+            time.sleep(config['MessageDelay'])
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
+    else:
+        exit("Finished Loop")
 
-    time.sleep(config['MessageDelay'])
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+if config['script_loop'] == False:
+    while keyboard.is_pressed(config['stop_key']) == False:
+        sb = open("script.txt", 'r')
 
-if config['DoneMessage'] == 'true':
-    pyautogui.typewrite("Done!")
+        for word in sb:
+            time.sleep(config['MessageDelay'])
+            pyautogui.typewrite(word)
+            pyautogui.press("enter")
+        exit("Finished!")
+
